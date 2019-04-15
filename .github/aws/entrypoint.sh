25514fec4d8d532bb5b07c006de08fde7ddac83f
Shawn Mclean <shawnrmclean@gmail.com>
2019-04-15 07:00:40 -0400

    Change session name.

--- .github/aws/entrypoint.sh
+++ .github/aws/entrypoint.sh
@@ -12,7 +12,7 @@ unset AWS_SESSION_TOKEN
 
 temp_role=$(aws sts assume-role \
                     --role-arn "$ROLE_ARN" \
-                    --role-session-name "$SESSION_NAME".)
+                    --role-session-name "temp-session".)
 
 export AWS_ACCESS_KEY_ID=$(echo $temp_role | jq .Credentials.AccessKeyId | xargs)
 export AWS_SECRET_ACCESS_KEY=$(echo $temp_role | jq .Credentials.SecretAccessKey | xargs)
