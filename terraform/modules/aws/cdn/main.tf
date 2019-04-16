c4a511831af3d9af85acf73a6b466080df4dd9e7
Shawn Mclean <shawnrmclean@gmail.com>
2019-04-16 11:44:32 -0400

    Remove s3_origin_config.

--- terraform/modules/aws/cdn/main.tf
+++ terraform/modules/aws/cdn/main.tf
@@ -6,10 +6,6 @@ resource "aws_cloudfront_distribution" "s3_distribution" {
   origin {
     domain_name = "${var.s3_bucket_regional_domain_name}"
     origin_id   = "${local.s3_origin_id}"
-
-    s3_origin_config {
-      origin_access_identity = "origin-access-identity/cloudfront/ABCDEFG1234567"
-    }
   }
 
   enabled             = true
