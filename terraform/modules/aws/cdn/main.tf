55bf12d875513f33fc494f2d310db5c7b083492c
Shawn Mclean <shawnrmclean@gmail.com>
2019-04-20 14:02:34 -0500

    Remove cert and domain alias from CloudFront.

--- terraform/modules/aws/cdn/main.tf
+++ terraform/modules/aws/cdn/main.tf
@@ -10,10 +10,10 @@ resource "aws_cloudfront_distribution" "s3_distribution" {
 
   enabled             = true
   is_ipv6_enabled     = true
-  comment             = "Some comment"
+  comment             = "AWS Landing Page CDN"
   default_root_object = "index.html"
 
-  aliases = ["samelogic.com", "www.samelogic.com"]
+  //aliases = ["samelogic.com", "www.samelogic.com"]
 
   default_cache_behavior {
     allowed_methods  = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
@@ -33,7 +33,6 @@ resource "aws_cloudfront_distribution" "s3_distribution" {
     default_ttl            = 3600
     max_ttl                = 86400
   }
-
   # Cache behavior with precedence 0
   ordered_cache_behavior {
     path_pattern     = "/content/immutable/*"
@@ -56,7 +55,6 @@ resource "aws_cloudfront_distribution" "s3_distribution" {
     compress               = true
     viewer_protocol_policy = "redirect-to-https"
   }
-
   # Cache behavior with precedence 1
   ordered_cache_behavior {
     path_pattern     = "/content/*"
@@ -78,20 +76,21 @@ resource "aws_cloudfront_distribution" "s3_distribution" {
     compress               = true
     viewer_protocol_policy = "redirect-to-https"
   }
-
   price_class = "PriceClass_200"
-
   restrictions {
     geo_restriction {
       restriction_type = "none"
     }
   }
-
   tags {
     SERVICE = "${var.service_name}"
   }
-
+  # viewer_certificate {
+  #   acm_certificate_arn      = "	arn:aws:acm:us-east-1:232825056036:certificate/79862da6-c711-4dbf-ab14-0d23b199aaee"
+  #   minimum_protocol_version = "TLSv1.1_2016"
+  #   ssl_support_method       = "sni-only"
+  # }
   viewer_certificate {
-    acm_certificate_arn = "arn:aws:acm:us-east-1:024334758614:certificate/87c37829-02da-43f7-9315-6aae0a66ed27"
+    cloudfront_default_certificate = true
   }
 }
