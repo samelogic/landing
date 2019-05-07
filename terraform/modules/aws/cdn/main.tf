d909352bc3f21a0cc35ea082d4f9cfbf7c756cfe
Shawn Mclean <shawnrmclean@gmail.com>
2019-05-07 02:23:30 -0500

    Add certificate.

--- terraform/modules/aws/cdn/main.tf
+++ terraform/modules/aws/cdn/main.tf
@@ -10,7 +10,7 @@ resource "aws_cloudfront_distribution" "s3_distribution" {
 
   enabled             = true
   is_ipv6_enabled     = true
-  comment             = "AWS Landing Page CDN"
+  comment             = "Terraform AWS Landing Page CDN"
   default_root_object = "index.html"
 
   aliases = ["samelogic.com", "www.samelogic.com"]
@@ -33,6 +33,7 @@ resource "aws_cloudfront_distribution" "s3_distribution" {
     default_ttl            = 3600
     max_ttl                = 86400
   }
+
   # Cache behavior with precedence 0
   ordered_cache_behavior {
     path_pattern     = "/content/immutable/*"
@@ -55,6 +56,7 @@ resource "aws_cloudfront_distribution" "s3_distribution" {
     compress               = true
     viewer_protocol_policy = "redirect-to-https"
   }
+
   # Cache behavior with precedence 1
   ordered_cache_behavior {
     path_pattern     = "/content/*"
@@ -76,16 +78,21 @@ resource "aws_cloudfront_distribution" "s3_distribution" {
     compress               = true
     viewer_protocol_policy = "redirect-to-https"
   }
+
   price_class = "PriceClass_200"
+
   restrictions {
     geo_restriction {
       restriction_type = "none"
     }
   }
+
   tags {
     SERVICE = "${var.service_name}"
   }
+
   viewer_certificate {
-    cloudfront_default_certificate = true
+    acm_certificate_arn = "arn:aws:acm:us-east-1:284148280762:certificate/4dc6f6df-efbb-47c0-9568-28432bd62053"
+    ssl_support_method  = "sni-only"
   }
 }
