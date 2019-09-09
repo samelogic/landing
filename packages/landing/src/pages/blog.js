d89ae07dd4c4f26bc4c3ce4e6d6fbd3d2e1d5603
Shawn Mclean <shawnrmclean@gmail.com>
2019-09-09 02:41:54 -0500

    Add style to blog post list.

--- packages/landing/src/pages/blog.js
+++ packages/landing/src/pages/blog.js
@@ -1,26 +1,51 @@
-import React from 'react';
-import { Link, graphql } from 'gatsby';
-import Layout from '../components/layout';
+import React, { Fragment } from 'react';
+import Sticky from 'react-stickynode';
+import { ThemeProvider } from 'styled-components';
+import { saasTheme } from 'common/src/theme/saas';
+import { ResetCSS } from 'common/src/assets/css/style';
+import Container from 'common/src/components/UI/Container';
+import { GlobalStyle, ContentWrapper } from '../containers/Saas/saas.style';
+import Navbar from '../containers/Saas/Navbar';
+import Footer from '../containers/Saas/Footer';
+import { DrawerProvider } from 'common/src/contexts/DrawerContext';
 import SEO from '../components/seo';
-const BlogPosts = ({ data }) => {
+import { Link, graphql } from 'gatsby';
+
+export default ({ data }) => {
   const blogPosts = data.allContentfulPost.edges;
   return (
-    <Layout>
-      <SEO title="Blog posts" />
-      <h1>{"Here's a list of all blogposts!"}</h1>
-      <div className="blogposts">
-        {blogPosts.map(({ node: post }) => (
-          <div key={post.id}>
-            <Link to={`/blog/${post.slug}`}>{post.title}</Link>
+    <ThemeProvider theme={saasTheme}>
+      <Fragment>
+        <SEO title="Samelogic Blog" />
+        <ResetCSS />
+        <GlobalStyle />
+        <ContentWrapper>
+          <Sticky top={0} innerZ={9999} activeClass="sticky-nav-active">
+            <DrawerProvider>
+              <Navbar />
+            </DrawerProvider>
+          </Sticky>
+          <div style={{ paddingTop: '210px', paddingBottom: '160px' }}>
+            <Container>
+              <h1>{"Here's a list of all blogposts!"}</h1>
+              <div className="blogposts">
+                {blogPosts.map(({ node: post }) => (
+                  <div key={post.id}>
+                    <Link to={`/blog/${post.slug}`}>{post.title}</Link>
+                  </div>
+                ))}
+                <span className="mgBtm__24" />
+                <Link to="/">Go back to the homepage</Link>
+              </div>
+            </Container>
           </div>
-        ))}
-        <span className="mgBtm__24" />
-        <Link to="/">Go back to the homepage</Link>
-      </div>
-    </Layout>
+          <Footer />
+        </ContentWrapper>
+      </Fragment>
+    </ThemeProvider>
   );
 };
-export default BlogPosts;
+
 export const query = graphql`
   query BlogPostsPageQuery {
     allContentfulPost(limit: 1000) {
