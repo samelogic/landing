81489ff9388b359a82c780cfab3147c45e5b360b
Shawn Mclean <shawnrmclean@gmail.com>
2019-09-13 21:45:14 -0500

    Move blog list into own container.

--- packages/landing/src/pages/blog.js
+++ packages/landing/src/pages/blog.js
@@ -3,16 +3,14 @@ import Sticky from 'react-stickynode';
 import { ThemeProvider } from 'styled-components';
 import { saasTheme } from 'common/src/theme/saas';
 import { ResetCSS } from 'common/src/assets/css/style';
-import Container from 'common/src/components/UI/Container';
 import { GlobalStyle, ContentWrapper } from '../containers/Saas/saas.style';
 import Navbar from '../containers/Saas/Navbar';
 import Footer from '../containers/Saas/Footer';
+import BlogList from '../containers/Saas/BlogList';
 import { DrawerProvider } from 'common/src/contexts/DrawerContext';
 import SEO from '../components/seo';
-import { Link, graphql } from 'gatsby';
 
-export default ({ data }) => {
-  const blogPosts = data.allContentfulPost.edges;
+export default () => {
   return (
     <ThemeProvider theme={saasTheme}>
       <Fragment>
@@ -25,41 +23,10 @@ export default ({ data }) => {
               <Navbar />
             </DrawerProvider>
           </Sticky>
-          <div style={{ paddingTop: '210px', paddingBottom: '160px' }}>
-            <Container>
-              <h1>{"Here's a list of all blogposts!"}</h1>
-              <div className="blogposts">
-                {blogPosts.map(({ node: post }) => (
-                  <div key={post.id}>
-                    <Link to={`/blog/${post.slug}`}>{post.title}</Link>
-                  </div>
-                ))}
-                <span className="mgBtm__24" />
-                <Link to="/">Go back to the homepage</Link>
-              </div>
-            </Container>
-          </div>
+          <BlogList />
           <Footer />
         </ContentWrapper>
       </Fragment>
     </ThemeProvider>
   );
 };
-
-export const query = graphql`
-  query BlogPostsPageQuery {
-    allContentfulPost(limit: 1000) {
-      edges {
-        node {
-          id
-          title
-          slug
-          content {
-            content
-          }
-          tags
-        }
-      }
-    }
-  }
-`;
