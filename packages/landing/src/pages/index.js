baa858f8a886a9d2f2792a07d4ec77c3f506c17a
Dwayne Samuels <dwayne@samelogic.com>
2019-09-27 22:16:07 -0700

    Feature section removed

--- packages/landing/src/pages/index.js
+++ packages/landing/src/pages/index.js
@@ -18,7 +18,6 @@ export default ({ path }) => {
       <SEO title="Samelogic | Agile experiments in your SaaS products" />
 
       <BannerSection />
-      <FeatureSection />
       <VisitorSection />
       <ServiceSection />
       <PricingSection />
