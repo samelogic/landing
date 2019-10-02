b63af92562af9be8505daedd1132eba05ea68d13
Dwayne Samuels <dwayne@samelogic.com>
2019-10-01 23:33:02 -0700

    ToS updated

--- packages/landing/src/containers/TermsPage/index.tsx
+++ packages/landing/src/containers/TermsPage/index.tsx
@@ -2,7 +2,10 @@ import * as React from 'react';
 import Container from 'common/src/components/UI/Container';
 
 const TermsPage = () => {
-  return <Container>Terms of Service</Container>;
+  return <Container>
+    <div>Terms of Service</div>
+    <div>We're working with a lawyer to make this concise and easy to understand</div>
+    </Container>;
 };
 
 export default TermsPage;
