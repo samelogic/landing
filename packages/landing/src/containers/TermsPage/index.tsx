cbec6b9ce28373bd902a147482a01e4e7ff329db
Dwayne Samuels <dwayne@samelogic.com>
2019-10-02 23:00:15 -0700

    Terms updated

--- packages/landing/src/containers/TermsPage/index.tsx
+++ packages/landing/src/containers/TermsPage/index.tsx
@@ -4,7 +4,7 @@ import Container from 'common/src/components/UI/Container';
 const TermsPage = () => {
   return <Container>
     <div>Terms of Service</div>
-    <div>We're working with a lawyer to make this concise and easy to understand</div>
+    <div>We're working to make this concise and easy to understand</div>
     </Container>;
 };
 
