a0c384a70df0526bc2e25421993a02b81dd2bd02
Shawn Mclean <shawnrmclean@gmail.com>
2019-11-04 23:27:24 -0800

    Add form behavior.

--- packages/landing/src/components/Survey/SurveyForm.tsx
+++ packages/landing/src/components/Survey/SurveyForm.tsx
@@ -1,62 +1,60 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useFormik } from 'formik';
 import { makeStyles, Theme, createStyles } from '@material-ui/core/styles';
+import ToggleInput from './ToggleInput';
 
-const useStyles = makeStyles((theme: Theme) =>
-  createStyles({
-  })
-);
+const useStyles = makeStyles((theme: Theme) => createStyles({}));
 
-type SurveyFormProps = {
-};
+type SurveyFormProps = {};
 
-const SurveyForm: React.FunctionComponent<SurveyFormProps> = ({
-  ...props
-}) => {
+const SurveyForm: React.FunctionComponent<SurveyFormProps> = ({ ...props }) => {
+  const [isPmSet, setPM] = useState(false);
+
   // Notice that we have to initialize ALL of fields with values. These
   // could come from props, but since we don't want to prefill this form,
   // we just use an empty string. If you don't do this, React will yell
   // at you.
   const formik = useFormik({
     initialValues: {
-      firstName: '',
-      lastName: '',
-      email: '',
+      isPM: false,
+      email: ''
     },
     onSubmit: values => {
       alert(JSON.stringify(values, null, 2));
-    },
+    }
   });
+
+  const handleToggleChange = (
+    event: React.MouseEvent<HTMLElement>,
+    value: any
+  ) => {
+    formik.handleChange({
+      target: {
+        name: 'isPM',
+        value: value
+      }
+    } as React.ChangeEvent<any>);
+    setPM(true);
+  };
+
   return (
     <form onSubmit={formik.handleSubmit}>
-      <label htmlFor="firstName">First Name</label>
-      <input
-        id="firstName"
-        name="firstName"
-        type="text"
-        onChange={formik.handleChange}
-        value={formik.values.firstName}
-      />
-      <label htmlFor="lastName">Last Name</label>
-      <input
-        id="lastName"
-        name="lastName"
-        type="text"
-        onChange={formik.handleChange}
-        value={formik.values.lastName}
-      />
-      <label htmlFor="email">Email Address</label>
-      <input
-        id="email"
-        name="email"
-        type="email"
-        onChange={formik.handleChange}
-        value={formik.values.email}
-      />
-      <button type="submit">Submit</button>
+      <label htmlFor="isPM">Are you a Product Manager?</label>
+      <ToggleInput value={formik.values.isPM} onChange={handleToggleChange} />
+      {isPmSet ? (
+        <>
+          <label htmlFor="email">Whats your email?</label>
+          <input
+            id="email"
+            name="email"
+            type="text"
+            onChange={formik.handleChange}
+            value={formik.values.email}
+          />
+          <button type="submit">Submit</button>
+        </>
+      ) : null}
     </form>
   );
 };
 export default SurveyForm;
-
-
