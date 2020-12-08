f81f6f8664591b98a81619f6bd48f0dda1b265f9
Nicolas Brown <nclsbrwn@gmail.com>
2020-12-08 12:01:26 -0500

    add initial setup for contentful

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,3 +1,6 @@
+const Promise = require('bluebird')
+const path = require('path')
+
 exports.onCreatePage = ({ page, actions }) => {
   const { createPage } = actions;
 
@@ -6,3 +9,43 @@ exports.onCreatePage = ({ page, actions }) => {
     createPage(page);
   }
 };
+
+exports.createPages = ({ graphql, actions }) => {
+  const { createPage } = actions
+
+  return new Promise((resolve, reject) => {
+    const blogPost = path.resolve('./src/pages/blog-detail.js')
+    resolve(
+      graphql(
+        `
+          {
+            allContentfulBlogPost {
+              edges {
+                node {
+                  title
+                  slug
+                }
+              }
+            }
+          }
+          `
+      ).then(result => {
+        if (result.errors) {
+          console.log(result.errors)
+          reject(result.errors)
+        }
+
+        const posts = result.data.allContentfulBlogPost.edges
+        posts.forEach((post, index) => {
+          createPage({
+            path: `/blog/${post.node.slug}/`,
+            component: blogPost,
+            context: {
+              slug: post.node.slug
+            },
+          })
+        })
+      })
+    )
+  })
+}
