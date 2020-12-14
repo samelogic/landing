569200426ca38a61d50d9fe194ee8c9ef1897bb6
dwayne <dwayne@samelogic.com>
2020-12-13 21:28:06 -0500

    dark Samelogic logo added

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,16 +1,16 @@
 import React from "react";
 import { Link } from "gatsby";
 
-import imgL1Logo from "../../assets/image/png/l1-logo.png";
+import imgL1Logo from "../../assets/image/svg/Official-Samelogic-Logo-dark.svg";
 import imgL1LogoWhite from "../../assets/image/svg/Official-Samelogic-Logo-white.svg";
 
 const Logo = ({ white, height, className = "", ...rest }) => {
   return (
     <Link to="/" className={`${className}`} {...rest}>
       {white ? (
-        <img src={imgL1LogoWhite} alt="" />
+        <img src={imgL1LogoWhite} alt="Samelogic Logo" />
       ) : (
-        <img src={imgL1Logo} alt="" />
+        <img src={imgL1Logo} alt="Samelogic Logo" />
       )}
     </Link>
   );
