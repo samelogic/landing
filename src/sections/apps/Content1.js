f34054b8d362f660816e000604a53ea9e0416eb4
Shawn Mclean <shawnrmclean@gmail.com>
2021-10-08 22:06:53 -0500

    Do not return content sector without description.

--- src/sections/apps/Content1.js
+++ src/sections/apps/Content1.js
@@ -22,47 +22,50 @@ const ShapeCard = styled(Box)`
   }
 `;
 
-const Content1 = ({ description }) => (
-  <>
-    {/* <!-- Content section 1 --> */}
-    <Section>
-      <Container>
-        <Row className="align-items-center">
-          <Col md="12" lg="6" className="order-lg-2">
-            <div className="position-relative pl-lg-5">
+const Content1 = ({ description }) => {
+  if (!description) return <></>;
+  return (
+    <>
+      {/* <!-- Content section 1 --> */}
+      <Section>
+        <Container>
+          <Row className="align-items-center">
+            <Col md="12" lg="6" className="order-lg-2">
+              <div className="position-relative pl-lg-5">
+                <div
+                  className="pl-lg-4 text-center text-lg-right"
+                  data-aos="fade-down"
+                  data-aos-duration="750"
+                  data-aos-once="true"
+                >
+                  <img src={imgContentMobile} alt="" className="img-fluid" />
+                </div>
+              </div>
+            </Col>
+            <Col lg="6" className="order-lg-1 mt-5 mt-lg-0">
               <div
-                className="pl-lg-4 text-center text-lg-right"
-                data-aos="fade-down"
+                data-aos="fade-right"
                 data-aos-duration="750"
                 data-aos-once="true"
               >
-                <img src={imgContentMobile} alt="" className="img-fluid" />
-              </div>
-            </div>
-          </Col>
-          <Col lg="6" className="order-lg-1 mt-5 mt-lg-0">
-            <div
-              data-aos="fade-right"
-              data-aos-duration="750"
-              data-aos-once="true"
-            >
-              <Box>
-                <Title>{description.title}</Title>
-                <Text
-                  mb={4}
-                  dangerouslySetInnerHTML={{ __html: description.body }}
-                />
+                <Box>
+                  <Title>{description.title}</Title>
+                  <Text
+                    mb={4}
+                    dangerouslySetInnerHTML={{ __html: description.body }}
+                  />
 
-                <a href="https://app.samelogic.com">
-                  <Button mt={3}>Get Started</Button>
-                </a>
-              </Box>
-            </div>
-          </Col>
-        </Row>
-      </Container>
-    </Section>
-  </>
-);
+                  <a href="https://app.samelogic.com">
+                    <Button mt={3}>Get Started</Button>
+                  </a>
+                </Box>
+              </div>
+            </Col>
+          </Row>
+        </Container>
+      </Section>
+    </>
+  );
+};
 
 export default Content1;
