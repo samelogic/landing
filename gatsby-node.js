a1183e57be8b648f1f15781e332d197efff4fcb0
Shawn Mclean <shawnmclean@users.noreply.github.com>
2021-10-26 18:48:57 -0500

    Sort blog by createdAt date. (#27)
    * Sort blog by createdAt date.
    
    * fix fields prop in post sort
    
    * Fix sorting on blog list
    
    Co-authored-by: Nicolas Brown <nclsbrwn@gmail.com>

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,5 +1,5 @@
-const Promise = require('bluebird')
-const path = require('path')
+const Promise = require("bluebird");
+const path = require("path");
 
 exports.onCreatePage = ({ page, actions }) => {
   const { createPage } = actions;
@@ -11,10 +11,10 @@ exports.onCreatePage = ({ page, actions }) => {
 };
 
 exports.createPages = ({ graphql, actions }) => {
-  const { createPage } = actions
+  const { createPage } = actions;
 
   return new Promise((resolve, reject) => {
-    const blogPost = path.resolve('./src/templates/blog-details.js')
+    const blogPost = path.resolve("./src/templates/blog-details.js");
     resolve(
       graphql(
         `
@@ -28,24 +28,24 @@ exports.createPages = ({ graphql, actions }) => {
               }
             }
           }
-          `
-      ).then(result => {
+        `
+      ).then((result) => {
         if (result.errors) {
-          console.log(result.errors)
-          reject(result.errors)
+          console.log(result.errors);
+          reject(result.errors);
         }
 
-        const posts = result.data.allContentfulPost.edges
+        const posts = result.data.allContentfulPost.edges;
         posts.forEach((post, index) => {
           createPage({
             path: `/blog/${post.node.slug}/`,
             component: blogPost,
             context: {
-              slug: post.node.slug
+              slug: post.node.slug,
             },
-          })
-        })
+          });
+        });
       })
-    )
-  })
-}
+    );
+  });
+};
