f48f574bcc87949a0b06e66037487f327af7a7bc
Shawn Mclean <shawnmclean@users.noreply.github.com>
2022-04-07 16:23:24 -0500

    Update microsurvey copy. (#36)

--- src/pages/apps/microsurveys.js
+++ src/pages/apps/microsurveys.js
@@ -1,64 +1,67 @@
-import React from 'react'
-import Hero from '../../sections/apps/Hero'
-import Clients from '../../sections/apps/Clients'
-import Feature from '../../sections/apps/Feature'
-import Content1 from '../../sections/apps/Content1'
-import Content2 from '../../sections/apps/Content2'
-import Testimonial from '../../sections/apps/Testimonial'
-import CTA from '../../sections/apps/CTA'
-import PageWrapper from '../../components/PageWrapper'
+import React from "react";
+import Hero from "../../sections/apps/Hero";
+import Clients from "../../sections/apps/Clients";
+import Feature from "../../sections/apps/Feature";
+import Content1 from "../../sections/apps/Content1";
+import Content2 from "../../sections/apps/Content2";
+import Testimonial from "../../sections/apps/Testimonial";
+import CTA from "../../sections/apps/CTA";
+import PageWrapper from "../../components/PageWrapper";
 
 const APP_DATA = {
-  name: 'Microsurveys',
+  name: "Microsurveys",
   introduction:
-    'Microsurveys are powerful but short in-app product surveys to collect contextual feedback, such as user flows and features.',
-  heroImage: '',
+    "Microsurveys are powerful but short in-app product surveys to collect contextual feedback, such as user flows and features.",
+  heroImage: "",
   // icon: https://fontawesome.com/v5.15/icons?d=gallery&p=2&q=smile&m=free
   features: [
     {
-      icon: 'arrows-alt',
-      text: 'Position anywhere',
+      icon: "arrows-alt",
+      text: "Position anywhere",
     },
     {
-      icon: 'question',
-      text: '3+ question types',
+      icon: "question",
+      text: "3+ question types",
     },
     {
-      icon: 'palette',
-      text: 'Configure appearance',
+      icon: "palette",
+      text: "Configure appearance",
     },
     {
-      icon: 'code-branch',
-      text: 'Question logic',
+      icon: "code-branch",
+      text: "Question logic",
     },
     {
-      icon: 'smile-beam',
-      text: 'CSAT, CES, NPS',
+      icon: "smile-beam",
+      text: "CSAT, CES, NPS",
     },
   ],
-  categories: ['surveys', 'analytics'],
+  categories: ["surveys", "analytics"],
   testimonials: [
     {
-      name: '',
-      title: '',
-      statement: '',
+      name: "",
+      title: "",
+      statement: "",
     },
   ],
   description: {
-    title: 'Addtional Description',
+    title: "Microsurveys Rock",
     body: `
-    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-    Quis imperdiet massa tincidunt nunc. Dolor morbi non arcu risus quis varius quam. Eu volutpat odio facilisis mauris sit.
+    Microsurveys are a great way to gather contextual user research data that can be used immediately to validate ideas. 
+    With Samelogic, you can create beautiful,  microsurveys that are easy to deploy and collect responses from. 
+    We're confident that you'll find our platform to be the perfect tool for collecting quick, easy feedback from your users.
+
     <br/> <br/>
-    Ultrices gravida dictum fusce ut placerat. Auctor elit sed vulputate mi sit amet. Eget est lorem ipsum dolor sit amet. Eget dolor morbi non arcu risus. Senectus et netus et malesuada. Morbi leo urna molestie at elementum eu facilisis sed. Quis risus sed vulputate odio ut enim blandit volutpat maecenas. Quisque id diam vel quam elementum pulvinar etiam. Mattis rhoncus urna neque viverra justo. Amet dictum sit amet justo donec enim diam. Bibendum neque egestas congue quisque. Nec feugiat nisl pretium fusce id.
+    Our team is always happy to help you get the most out of your Samelogic account. So if you have any questions, 
+    don't hesitate to reach out! We're here to help you succeed.
     `,
-    image: '',
+    image: "",
   },
   publisher: {
-    name: 'Samelogic Inc.',
-    logo: '',
+    name: "Samelogic Inc.",
+    logo: "",
   },
-}
+};
 
 const IndexPage = () => {
   return (
@@ -73,6 +76,6 @@ const IndexPage = () => {
         <CTA />
       </PageWrapper>
     </>
-  )
-}
-export default IndexPage
+  );
+};
+export default IndexPage;
