01478b9079c3c198140ad3ac123e84fb2f6127eb
Dwayne <dwayne@samelogic.com>
2022-08-31 11:09:13 -0400

    Pricing page updated (#39)
    * pricing link added
    
    * Stripe added to Pricing page
    
    * content for pricing page updated

--- src/pages/pricing.js
+++ src/pages/pricing.js
@@ -5,16 +5,18 @@ import Hero from "../sections/common/Hero";
 import Feature from "../sections/pricing/Feature";
 import PricingTable1 from "../sections/pricing/PricingTable1";
 import FAQ from "../sections/pricing/FAQ";
+import PricingTable2 from '../sections/pricing/PricingTable2';
 
 const Pricing1 = () => {
   return (
     <>
       <PageWrapper footerDark>
-        {/* <Hero title="Pricing">
-          Validate Your Product Ideas with Embedded Mockups.
-        </Hero> */}
+         <Hero title="Pricing">
+          Whatever your need may be, we have Plan that can help.
+        </Hero>
         <PricingTable1 />
-        {/* <Feature /> */}
+        {/* <PricingTable2/> */}
+        <Feature />
         {/* <FAQ /> */}
       </PageWrapper>
     </>
