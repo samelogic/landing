0ab03874f2081e545f5b415ed6b3f9920ba3d78b
Shawn Mclean <shawnrmclean@gmail.com>
2022-09-18 11:48:01 -0500

    Change Amplitude hero layout

--- src/pages/integrations/components/Hero.js
+++ src/pages/integrations/components/Hero.js
@@ -1,53 +1,101 @@
-import React from "react";
+import React, { useContext } from "react";
 import styled from "styled-components";
 import { Container, Row, Col } from "react-bootstrap";
 
-import { Title, Section, Box, Text } from "../../../components/Core";
-import CTAButton from "../../../components/CTAButton/CTAButton";
+import { Title, Button, Section, Box, Text } from "../../../components/Core";
+import CTAButton from "../../../components/CTAButton";
 
-const SectionStyled = styled(Section)``;
+import { device } from "../../../utils";
+
+const ShapeTopLeft = styled.div`
+  position: absolute;
+  left: 0;
+  top: 0;
+  transform: translate(-50%, -50%);
+`;
+
+const TopCard = styled(Box)`
+  width: 305px;
+  box-shadow: ${({ theme }) => `0 32px 34px ${theme.colors.shadow}`};
+  position: absolute;
+  top: 14%;
+  left: 0;
+  @media ${device.md} {
+    left: -13%;
+  }
+  @media ${device.lg} {
+    left: 2%;
+  }
+`;
+
+const BottomCard = styled(Box)`
+  width: 305px;
+  box-shadow: ${({ theme }) => `0 32px 34px ${theme.colors.shadow}`};
+  position: absolute;
+  bottom: 12%;
+  right: 0;
+  @media ${device.md} {
+    right: -13%;
+  }
+  @media ${device.lg} {
+    right: -5%;
+  }
+  @media ${device.xl} {
+    right: -33%;
+  }
+`;
+
+const ImgRight = styled.img`
+  max-width: 50%;
+  @media ${device.sm} {
+    max-width: 60%;
+  }
+  @media ${device.lg} {
+    max-width: 100%;
+  }
+`;
 
 const Hero = ({ img, title, body }) => {
   return (
-    <SectionStyled className="pb-0">
-      <div className="pt-5"></div>
-      <Container>
-        <Row className="justify-content-center text-center">
-          <Col lg="8">
-            <Box className="text-center" mb={4}>
-              <img src={img} alt="" className="img-fluid" />
-            </Box>
-            <Title variant="hero">
-              Send contextual user events to Amplitude
-            </Title>
-          </Col>
-        </Row>
+    <>
+      {/* <!-- Hero Area --> */}
+      <Section>
+        <div className="pt-5"></div>
 
-        <Row className="justify-content-center text-center">
-          <Col lg="6">
-            <Box className="text-center">
-              <Text>
-                Send your quantitative and qualitative user research events to
-                Amplitude. Dig deeper into the how and why are users interacting
-                with your feature test.
-              </Text>
-            </Box>
-            <Box className="text-center" mt={4}>
-              <CTAButton />
-            </Box>
-          </Col>
-        </Row>
-        <Row className="justify-content-center text-center">
-          <Col lg="10">
-            <Box mt={6}>
-              <Title>
-                Ensure you have all the evidence before building that feature!
-              </Title>
-            </Box>
-          </Col>
-        </Row>
-      </Container>
-    </SectionStyled>
+        <Container>
+          <Row className="justify-content-center align-items-center">
+            <Col lg="5" md="8" sm="9" className="order-lg-2">
+              <div className="text-center text-lg-right position-relative">
+                <div
+                  className="img-main"
+                  data-aos="fade-down"
+                  data-aos-duration="750"
+                  data-aos-once="true"
+                  data-aos-delay="500"
+                >
+                  <ImgRight src={img} alt="" />
+                </div>
+              </div>
+            </Col>
+            <Col lg="7" className="order-lg-1">
+              <div
+                data-aos="fade-right"
+                data-aos-duration="750"
+                data-aos-once="true"
+                data-aos-delay="500"
+              >
+                <Box py={[null, null, null, 5]} pr={5} pt={[4, null]}>
+                  <Title variant="hero">{title}</Title>
+                  <Text mb={4}>{body}</Text>
+
+                  <CTAButton mb={2} />
+                </Box>
+              </div>
+            </Col>
+          </Row>
+        </Container>
+      </Section>
+    </>
   );
 };
 
