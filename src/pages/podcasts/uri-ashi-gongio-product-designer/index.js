ce3e15664e7a8bfc92d9bc90e477a9c59da210fc
Shawn Mclean <shawnrmclean@gmail.com>
2022-10-14 11:38:16 -0500

    update uri

--- src/pages/podcasts/uri-ashi-gongio-product-designer/index.js
+++ src/pages/podcasts/uri-ashi-gongio-product-designer/index.js
@@ -4,11 +4,13 @@ import PageWrapper from "../../../components/PageWrapper";
 import Seo from "../../../components/SEO";
 import Hero from "../../../sections/podcasts/details/Hero";
 import CTA from "../../../sections/landing4/CTA";
-import imgHero from "../../../assets/image/png/landing4-hero-img.png";
+import imgHero from "./hero.jpg";
 import social from "./social.png";
 
 const LandingPage2 = ({ data }) => {
-  const title = "Growth and Monetization at Zendesk";
+  const title = "#29: Uri Ashi, Senior Product Designer at Gong.io";
+  const subtitle = "We spoke with Uri about product design";
+
   const body =
     "Uri Ashi is a senior product designer at Gong.io, the leading Revenue Intelligence platform, leveraging AI to go beyond traditional CRM and transform revenue teams. Besides that, Uri (pronounced Oori) is also a peace activist, a published author and illustrator, an award-winning animator, an entrepreneur (x2), a basso in a choir, a backing vocalist, and a guitar player in Gong’s band, a husband and a father.";
 
@@ -35,7 +37,7 @@ const LandingPage2 = ({ data }) => {
         <Hero
           date="Oct 14, 2022"
           title={title}
-          subtitle="Episode #29: Uri Ashi, Senior Product Designer at Gong.io"
+          subtitle={subtitle}
           spotifyId="7zpJnMFaBNzWZsNHXKYCJ7"
           heroImage={imgHero}
         />
