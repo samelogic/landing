4aa2551de4984250392ea0f3188a752c9bdd7ad7
Shawn Mclean <shawnrmclean@gmail.com>
2022-10-14 15:07:23 -0500

    add fareem

--- src/pages/podcasts/farheen-noorie-zendesk-director-product-manager/index.js
+++ src/pages/podcasts/farheen-noorie-zendesk-director-product-manager/index.js
@@ -1,49 +1,74 @@
 import React from "react";
 import { get } from "lodash";
+import { graphql, Link } from "gatsby";
+import { Container, Row, Col } from "react-bootstrap";
 import PageWrapper from "../../../components/PageWrapper";
 import Seo from "../../../components/SEO";
-import Hero from "../../../sections/podcasts/details/Hero";
-import CTA from "../../../sections/landing4/CTA";
-import imgHero from "../../../assets/image/png/landing4-hero-img.png";
+import { Section, Title, Text, Box } from "../../../components/Core";
+import imgHero from "./fareem.png";
+import PodcastDetails from "../../../sections/podcast/PodcastDetails";
 import social from "./social.png";
 
 const LandingPage2 = ({ data }) => {
-  const title =
-    "#28: Farheen Noorie, Director of Product Management Growth and Monetization at Zendesk";
-  const subtitle =
-    "We spoke with Farheen about her background in engineering and what it was like transitioning over to Product Management. Check out this exciting episode below.";
-
-  const body =
-    "Farheen has a background in engineering and worked at PayPal for many years before moving into Product Management at Zendesk. She currently leads Buying Experience, which is a part of the broader Growth and Monetization team. She is interested in anything related to growth and revenue drivers for a product, KPIs that matter vs. not, and solving complex business and technical problems. She is also interested in learning the guitar but has been procrastinating for over a year now.";
+  const podcast = {
+    title:
+      "#28: Farheen Noorie, Director of Product Management Growth and Monetization at Zendesk",
+    subtitle:
+      "We spoke with Farheen about her background in engineering and what it was like transitioning over to Product Management. Check out this exciting episode below.",
+    description:
+      "Farheen has a background in engineering and worked at PayPal for many years before moving into Product Management at Zendesk. She currently leads Buying Experience, which is a part of the broader Growth and Monetization team. She is interested in anything related to growth and revenue drivers for a product, KPIs that matter vs. not, and solving complex business and technical problems. She is also interested in learning the guitar but has been procrastinating for over a year now.",
+    content: {
+      heroImage: imgHero,
+      content: {
+        childMarkdownRemark: {
+          html: `
+      <iframe style="border-radius:12px" src="https://open.spotify.com/embed/episode/4smpLfFOKQruKHWIEaUmYu?utm_source=generator&theme=0" width="100%" height="152" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>`,
+        },
+      },
+    },
+  };
 
   const siteUrl = get(data, "site.siteMetadata.siteUrl");
   return (
     <>
-      {" "}
       <Seo
-        title={title}
-        description={body}
+        title={podcast.title + " | The Samelogic Podcast"}
+        description={podcast.description}
         twitterCard="summary_large_image"
         meta={[
           {
-            property: "twitter:image",
-            content: siteUrl + social,
+            name: `twitter:image`,
+            content: imgHero,
           },
           {
-            property: "og:image",
-            content: siteUrl + social,
+            property: `og:image`,
+            content: imgHero,
           },
         ]}
       />
-      <PageWrapper headerDark>
-        <Hero
-          date="Oct 14, 2022"
-          title={title}
-          subtitle={subtitle}
-          spotifyId="7zpJnMFaBNzWZsNHXKYCJ7"
-          heroImage={imgHero}
-        />
-        <CTA />
+      <PageWrapper footerDark>
+        <Section className="pb-4">
+          <div className="pt-5"></div>
+          <Container>
+            <Row className="justify-content-center text-center">
+              <Col lg="12">
+                <Title variant="hero">{podcast.title}</Title>
+                <Box className="d-flex justify-content-center">
+                  <Text mr={3}>{podcast.subtitle}</Text>
+                </Box>
+              </Col>
+            </Row>
+          </Container>
+        </Section>
+        <Section className="pb-8 pt-2">
+          <Container>
+            <Row>
+              <Col lg="8" className="mb-5">
+                <PodcastDetails post={podcast.content} />
+              </Col>
+            </Row>
+          </Container>
+        </Section>
       </PageWrapper>
     </>
   );
---
+++ src/sections/podcast/PodcastDetails.js
@@ -0,0 +1,117 @@
+import React from "react";
+import styled from "styled-components";
+import { getSrc } from "gatsby-plugin-image";
+
+import { Box, Badge } from "../../components/Core";
+import BlogCTA from "../blog/BlogCTA";
+
+import imgB1 from "../../assets/image/jpeg/blog-details-img-1.jpg";
+import iconQuote from "../../assets/image/png/quote-icon.png";
+
+const Post = styled(Box)`
+  font-size: 1rem;
+
+  h2,
+  h3,
+  h4,
+  h5,
+  h6,
+  p,
+  blockquote,
+  pre,
+  ul,
+  ol {
+    margin-bottom: 1.25rem;
+    &:last-child {
+      margin-bottom: 0;
+    }
+  }
+
+  h2,
+  h3,
+  h4,
+  h5,
+  h6 {
+    margin-top: 2.25rem;
+    margin-bottom: 1.25rem;
+    color: ${({ theme }) => theme.colors.heading} !important;
+  }
+
+  a {
+    text-decoration: underline;
+  }
+
+  ol li {
+    list-style-type: decimal;
+    margin-left: 1.25rem;
+  }
+
+  ul li {
+    list-style-type: disc;
+    margin-left: 1.25rem;
+  }
+
+  blockquote {
+    margin-bottom: 1.25rem;
+    padding-left: 50px;
+    position: relative;
+    color: ${({ theme }) => theme.colors.text} !important;
+    font-size: 20px;
+    &::after {
+      content: url(${iconQuote});
+      display: inline-block;
+      min-width: 28px;
+      max-width: 28px;
+      margin-top: 8px;
+      margin-right: 23px;
+      position: absolute;
+      top: 0;
+      left: 0;
+    }
+  }
+  img,
+  iframe,
+  video {
+    max-width: 100%;
+    margin-bottom: 2rem;
+    display: block;
+  }
+`;
+
+const BadgePost = ({ children }) => (
+  <Badge
+    mr={3}
+    mb={3}
+    bg="#eae9f2"
+    color="#696871"
+    fontSize="16px"
+    px={3}
+    py={2}
+  >
+    {children}
+  </Badge>
+);
+
+const PodcastDetails = ({ post }) => (
+  <>
+    {/* <!-- Blog section --> */}
+    <Post>
+      <div>
+        <img
+          className="pb-6"
+          style={{ borderRadius: "10px" }}
+          // src={getSrc(post.heroImage)}
+          src={post.heroImage}
+          alt=""
+        />
+      </div>
+      <div
+        dangerouslySetInnerHTML={{
+          __html: post.content.childMarkdownRemark.html,
+        }}
+      />
+    </Post>
+  </>
+);
+
+export default PodcastDetails;
