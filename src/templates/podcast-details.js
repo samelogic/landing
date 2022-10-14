8d85eb3e88514d4323c3b9b42cfbdcdaf61cc405
Shawn Mclean <shawnrmclean@gmail.com>
2022-10-14 16:44:51 -0500

    update

--- src/templates/podcast-details.js
+++ src/templates/podcast-details.js
@@ -11,6 +11,79 @@ import PodcastContent from "../sections/podcast/PodcastContent";
 import Comments from "../sections/blog/Comments";
 import Sidebar from "../sections/blog/Sidebar";
 import Seo from "../components/SEO";
+import styled from "styled-components";
+
+import iconQuote from "../assets/image/png/quote-icon.png";
+
+const Post = styled(Box)`
+  font-size: 1rem;
+
+  h2,
+  h3,
+  h4,
+  h5,
+  h6,
+  p,
+  blockquote,
+  pre,
+  ul,
+  ol {
+    margin-bottom: 1.25rem;
+    &:last-child {
+      margin-bottom: 0;
+    }
+  }
+
+  h2,
+  h3,
+  h4,
+  h5,
+  h6 {
+    margin-top: 2.25rem;
+    margin-bottom: 1.25rem;
+    color: ${({ theme }) => theme.colors.heading} !important;
+  }
+
+  a {
+    text-decoration: underline;
+  }
+
+  ol li {
+    list-style-type: decimal;
+    margin-left: 1.25rem;
+  }
+
+  ul li {
+    list-style-type: disc;
+    margin-left: 1.25rem;
+  }
+
+  blockquote {
+    margin-bottom: 1.25rem;
+    padding-left: 50px;
+    position: relative;
+    color: ${({ theme }) => theme.colors.text} !important;
+    font-size: 20px;
+    &::after {
+      content: url(${iconQuote});
+      display: inline-block;
+      min-width: 28px;
+      max-width: 28px;
+      margin-top: 8px;
+      margin-right: 23px;
+      position: absolute;
+      top: 0;
+      left: 0;
+    }
+  }
+  img,
+  iframe,
+  video {
+    max-width: 100%;
+    margin-bottom: 2rem;
+    display: block;
+  }
+`;
 
 const PodcastDetails = ({ data }) => {
   const siteTitle = get(data, "site.siteMetadata.blog.title");
@@ -52,7 +125,22 @@ const PodcastDetails = ({ data }) => {
           <Container>
             <Row>
               <Col lg="8" className="mb-5">
-                <PodcastContent post={podcast} />
+                <Post>
+                  <div>
+                    <img
+                      className="pb-6"
+                      style={{ borderRadius: "10px" }}
+                      src={getSrc(podcast.heroImage)}
+                      alt=""
+                    />
+                  </div>
+                  <div>{podcast.subTitle.subTitle}</div>
+                  <div
+                    dangerouslySetInnerHTML={{
+                      __html: podcast.content.childMarkdownRemark.html,
+                    }}
+                  />
+                </Post>
               </Col>
               <Col lg="4" className="">
                 <Sidebar />
