68aa3379a7b26fe779327caeffd1ec113f1ffa29
Shawn Mclean <shawnrmclean@gmail.com>
2022-11-25 16:23:42 -0500

    switch over to rich text

---
+++ src/components/Core/ContentfulRichText.js
@@ -0,0 +1,78 @@
+import React from "react";
+import styled from "styled-components";
+import slugify from "@sindresorhus/slugify";
+import { INLINES, BLOCKS, MARKS } from "@contentful/rich-text-types";
+import { renderRichText } from "gatsby-source-contentful/rich-text";
+import { GatsbyImage, getImage } from "gatsby-plugin-image";
+
+const createJumpLink = (children) => {
+  return <a href={`#${slugify(children[0])}`}>{children}</a>;
+};
+
+const options = {
+  renderMark: {
+    [MARKS.BOLD]: (text) => <b className="font-bold">{text}</b>,
+    [MARKS.ITALIC]: (text) => <i className="font-italic">{text}</i>,
+    [MARKS.UNDERLINE]: (text) => <u className="underline">{text}</u>,
+    [MARKS.CODE]: (text) => (
+      <code className="font-mono px-1 py-2 mx-1 bg-gray-100 rounded text-sm">
+        {text}
+      </code>
+    ),
+  },
+  renderNode: {
+    [INLINES.HYPERLINK]: (node, children) => (
+      <a
+        href={node.data.uri}
+        target="_blank"
+        rel="noreferrer"
+        className="text-brand-default underline"
+      >
+        {children}
+      </a>
+    ),
+    [BLOCKS.HEADING_1]: (node, children) => <h2>{children}</h2>,
+    [BLOCKS.HEADING_2]: (node, children) => {
+      return <h2>{createJumpLink(children)}</h2>;
+    },
+    [BLOCKS.HEADING_3]: (node, children) => <h3>{createJumpLink(children)}</h3>,
+    [BLOCKS.HEADING_4]: (node, children) => <h4>{createJumpLink(children)}</h4>,
+    [BLOCKS.HEADING_5]: (node, children) => <h5>{createJumpLink(children)}</h5>,
+    [BLOCKS.HEADING_6]: (node, children) => <h6>{createJumpLink(children)}</h6>,
+
+    [BLOCKS.OL_LIST]: (node, children) => <ol>{children}</ol>,
+    [BLOCKS.UL_LIST]: (node, children) => <ul>{children}</ul>,
+
+    [BLOCKS.LIST_ITEM]: (node, children) => <li>{children}</li>,
+    [BLOCKS.PARAGRAPH]: (node, children) => {
+      if (node.content[0].value === "") {
+        return <br />;
+      } else {
+        return <p>{children}</p>;
+      }
+    },
+    [BLOCKS.QUOTE]: (children) => (
+      <blockquote>
+        <>"{children.content[0].content[0].value}"</>
+      </blockquote>
+    ),
+    [BLOCKS.HR]: () => <hr className="mb-6" />,
+    [BLOCKS.EMBEDDED_ASSET]: (node) => {
+      const { gatsbyImageData, description } = node.data.target;
+
+      return (
+        <GatsbyImage
+          image={getImage(gatsbyImageData)}
+          alt={description}
+          className="mb-10"
+        />
+      );
+    },
+  },
+};
+
+const ContentfulRichText = ({ richText }) => {
+  return <div>{renderRichText(richText, options)}</div>;
+};
+
+export default ContentfulRichText;
--- src/components/Core/index.js
+++ src/components/Core/index.js
@@ -1,17 +1,18 @@
-import Box from './Box'
-import Title from './Title'
-import Text from './Text'
-import A from './A'
-import Badge from './Badge'
-import Button from './Button'
-import Section from './Section'
-import Input from './Input'
-import Checkbox from './Checkbox'
-import InputGroup from './InputGroup'
-import Select from './Select'
-import Switch from './Switch'
-import Span from './Span'
-import List from './List'
+import Box from "./Box";
+import Title from "./Title";
+import Text from "./Text";
+import A from "./A";
+import Badge from "./Badge";
+import Button from "./Button";
+import Section from "./Section";
+import Input from "./Input";
+import Checkbox from "./Checkbox";
+import InputGroup from "./InputGroup";
+import Select from "./Select";
+import Switch from "./Switch";
+import Span from "./Span";
+import List from "./List";
+import ContentfulRichText from "./ContentfulRichText";
 
 export {
   Box,
@@ -28,4 +29,5 @@ export {
   Switch,
   Span,
   List,
-}
+  ContentfulRichText,
+};
